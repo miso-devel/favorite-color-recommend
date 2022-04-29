8c2d3482c788faf840f1e2fe87c0bf234d3b9a47
nisi0929 <kengo279@icloud.com>
2022-04-30 02:41:02 +0900

    jest & typescript setting

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    roots: ['<rootDir>/src'],
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/src/__mocks__/fileMock.js',
+        '\\.(css|scss)$': '<rootDir>/src/__mocks__/styleMock.js',
+    },
+    testMatch: [
+        '**/__tests__/**/*.+(ts|tsx|js)',
+        '**/?(*.)+(spec|test).+(ts|tsx|js)',
+    ],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+}
